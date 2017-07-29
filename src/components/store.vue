6bba6777288f78f962101603d9c79baed1d7ac45
Yijun Mao <siren0413@gmail.com>
2017-07-28 23:24:26 -0700

    add store page

---
+++ src/components/store.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import {mapMutations, mapState} from 'vuex'
+
+  export default {
+    name: 'store',
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/login'
+import Store from '@/components/store'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
-    }
+    },
+    {
+      path: '/store',
+      name: 'Store',
+      component: Store
+    },
   ]
 })
